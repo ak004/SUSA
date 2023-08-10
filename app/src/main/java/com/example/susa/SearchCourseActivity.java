25826c1e9b410501910519b89f3968c219f9d666
mascuudmusse <masudmuse@tabaarakict.org>
2023-08-10 10:03:21 +0300

    done fewffidnfdf

---
+++ app/src/main/java/com/example/susa/SearchCourseActivity.java
@@ -0,0 +1,14 @@
+package com.example.susa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_course);
+    }
+}
